97ca05fd18bba86969452f2b2a85dc7b0c531b4b
mohamed-HAJJI0 <mhmdhajji@stu.scu.edu.cn>
2026-06-08 22:27:41 +0800

    some shits

--- packages/client/src/components/PageSizeWrapper.tsx
+++ packages/client/src/components/PageSizeWrapper.tsx
@@ -1,18 +1,11 @@
 import React, { ReactNode } from "react";
-import { Col, Container, Row } from "react-bootstrap";
 
 interface IPageSizeWrapperProps {
   children: ReactNode;
 }
 
 const PageSizeWrapper: React.FC<IPageSizeWrapperProps> = ({ children }) => {
-  return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col>{children}</Col>
-      </Row>
-    </Container>
-  );
+  return <div className="page-wrapper">{children}</div>;
 };
 
 export default PageSizeWrapper;
